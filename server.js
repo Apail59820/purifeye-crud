b4af3460ff32b7cb950eaabe6ac3c80fbba8b50a
Apail59820 <92113742+Apail59820@users.noreply.github.com>
2023-05-12 21:21:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const { exec } = require('child_process');
 
 var corsOptions = {
   origin: ["http://localhost:8081", "http://localhost"]
@@ -15,16 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-
-app.get('/interface/historique', (req, res) => {
-  exec('php interface/index.php', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Erreur lors de l'exécution de PHP : ${stderr}`);
-      return res.status(500).send('Erreur du serveur');
-    }
-    res.send(stdout);
-  });
-});
 
 db.sequelize.sync({ force: false } /* drop db : development */)
   .then(() => {
