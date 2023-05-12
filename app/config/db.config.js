5bb9756ff86e52964fb3c2afa8f46f732f441dab
Apail59820 <92113742+Apail59820@users.noreply.github.com>
2023-05-12 21:03:02 +0200

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "root",
-  DB: "entries",
+  HOST: "eu-cdbr-west-03.cleardb.net",
+  USER: "b5b39d52f466ad",
+  PASSWORD: "	eab3317c",
+  DB: "heroku_56366cbee5ccd56",
   dialect: "mysql",
   pool: {
     max: 5,
