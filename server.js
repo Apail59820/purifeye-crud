9d0446e15fb950414a7f1b43cdfd93dc9c780eda
Apail59820 <92113742+Apail59820@users.noreply.github.com>
2023-05-12 21:16:49 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const { exec } = require('child_process');
 
 var corsOptions = {
   origin: ["http://localhost:8081", "http://localhost"]
@@ -14,6 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
+
+app.get('/interface/historique', (req, res) => {
+  exec('php interface/index.php', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erreur lors de l'exécution de PHP : ${stderr}`);
+      return res.status(500).send('Erreur du serveur');
+    }
+    res.send(stdout);
+  });
+});
+
 db.sequelize.sync({ force: false } /* drop db : development */)
   .then(() => {
     console.log("Base de donnée synchonisée..");
